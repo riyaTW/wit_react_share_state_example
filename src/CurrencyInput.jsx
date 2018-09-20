0c659aff939d72d21e18893b1d7be83a7f0306dd
Riya Kataria <Riya_Kataria@external.mckinsey.com>
2018-09-20 21:38:36 +0530

    Added prop types to currency input component

--- src/CurrencyInput.jsx
+++ src/CurrencyInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class CurrencyInput extends React.Component {
 
@@ -6,7 +7,7 @@ class CurrencyInput extends React.Component {
         const {amount, currency} = this.props;
         return (
             <div>
-                <input type='text' name='currency' value={amount} onChange={this.props.changeAmount}/>
+                <input type='text' name='currency' value={amount || 0} onChange={this.props.changeAmount}/>
                 <select value={currency} onChange={this.props.changeCurrency}>
                     <option value="INR">Indian Rupee</option>
                     <option value="USD">US Dollar</option>
@@ -15,5 +16,10 @@ class CurrencyInput extends React.Component {
         );
     }
 }
+
+CurrencyInput.propTypes = {
+    amount: PropTypes.number,
+    currency: PropTypes.string.isRequired
+};
 
 export default CurrencyInput;
